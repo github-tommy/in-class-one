57a0f53f9944a84adaa133ad12784b935c0150e2
tom <tommy.surjadi@gmail.com>
2019-04-08 20:01:07 -0700

    updated both html and js files

--- lesson01/assignments/scripts.js
+++ lesson01/assignments/scripts.js
@@ -12,39 +12,14 @@ $(function () {
         const num = parseInt(val);
         // Verify the user gave us a real number
         if (!isNaN(num)) {
-          // Add our new number to the data array
-          data.push(num);
-          // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
-          const sum = data.reduce((acc, curr) => acc + curr);
-          // Add our new total to the page
-          resultSum.text(sum);
-          // Reset the input value to nothing
-          input.val('');
+            // Add our new number to the data array
+            // data.push(num);
+            // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
+            const sum = data.map((x) => x + num);
+            // Add our new total to the page
+            resultSum.text(sum);
+            // Reset the input value to an empty string
+            input.val('');
         }
     });
 });
-
-// ======ADDING THE NUMBER INPUTTED BY THE USER, BY 10
-
-$(function() {
-    const data = [1, 2, 3, 4],
-        input = $('.input'),
-        submit = $('.submit'),
-        resultMap = $('.map');
-    submit.on('click', function() {
-        const val = input.val();
-        const num = parseInt(val);
-
-        if (!isNaN(num)) {
-
-            data.push(num);
-
-            const map = data.map(function(x) {
-                return x + 10;
-            
-                resultMap.text(map);
-
-                input.val('');
-            });
-    });
-    });
